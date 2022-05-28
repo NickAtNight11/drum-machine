50ca675c400e1bd16398d53041b8c72f7adfa3ca
NickAtNight11 <nwatson7777@gmail.com>
2022-05-28 02:57:27 -0500

    Finished Sound Functions and Responsitivity

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from 'react'
+import { Howl, Howler } from 'howler';
 import './DrumMachine.css'
-
+import Clave from './sounds/Clave.wav';
+import ClosedHiHat from './sounds/Closed Hi-Hat.wav';
+import Cymbal from './sounds/Cymbal.wav';
+import HandClap from './sounds/Hand Clap.wav';
+import HighTom from './sounds/High Tom.wav';
+import Kick from './sounds/Kick.wav';
+import LowTom from './sounds/Low Tom.wav';
+import OpenHiHat from './sounds/Open Hi-Hat.wav';
+import Snare from './sounds/Snare.wav';
 
 function DrumMachine() {
 
     const [volume, setVolume] = useState(50);
     const [clip, setClip] = useState("");
-    const [drumsound, setDrumSound] = useState(null);
 
-    let clave = new Audio('./sounds/Clave.wav');
-    let closedhihat = new Audio('./sounds/Closed Hi-Hat.wav');
-    let cymbal = new Audio('./sounds/Cymbal.wav');
-    let handclap = new Audio('./sounds/Hand Clap.wav');
-    let hightom = new Audio('./sounds/High Tom.wav');
-    let kick = new Audio('./sounds/Kick.wav');
-    let lowtom = new Audio('./sounds/Low Tom.wav');
-    let openhihat = new Audio('./sounds/Open Hi-Hat.wav');
-    let snare = new Audio('./sounds/Snare.wav');
+    let clave = new Howl({src: Clave});
+    let closedhihat = new Howl({src: ClosedHiHat});
+    let cymbal = new Howl({src: Cymbal});
+    let handclap = new Howl({src: HandClap});
+    let hightom = new Howl({src: HighTom});
+    let kick = new Howl({src: Kick});
+    let lowtom = new Howl({src: LowTom});
+    let openhihat = new Howl({src: OpenHiHat});
+    let snare = new Howl({src: Snare});
+
+    let sounds = [kick, snare, handclap, closedhihat, openhihat, cymbal, lowtom, hightom, clave];
 
     useEffect(() => {
         window.addEventListener("keydown", keyPressed);
@@ -29,38 +39,26 @@ function DrumMachine() {
         setVolume(e.currentTarget.value);
     });
 
-    let changeClip = ((e) => { 
-        setClip(e.target.getAttribute("name"));
-        setDrumSound(e.target.getAttribute("sound"));
-        playClip();
+    let playClip = ((e, s) => {
+        setClip(e.getAttribute("name"));
+        Howler.volume(volume/100);
+        s.play();
     });
       
     let keyPressed = ((e) => {
         let k = String.fromCharCode(e.keyCode)
         for(let i=0; i<9; i++)
         {
-            if(document.getElementById("pad-grid").children[i].getAttribute("padkey") === k)
+            if(document.getElementById("pad-grid").children[i].children[0].innerHTML === k)
             {
-                let a = document.getElementById("pad-grid").children[i]
-                setClip(a.target.getAttribute("name"));
-                setDrumSound(a.target.getAttribute("sound"));
-                playClip();
+                playClip(document.getElementById("pad-grid").children[i], sounds[i]);
                 break;
             }
         }
     });
       
-    let playClip = (() => {
-        drumsound.volume = volume/100;
-        drumsound.play();
-        if(!drumsound.paused) {
-          drumsound.currentTime = 0;
-        }
-    });
-      
-    
     return (
-        <div id="background"> {/*onMouseUp={stopDrag} >*/}
+        <div id="background">
             <div id="interface">
                 <div id="output">
                 <h1 id="volume-output">{volume}</h1>
@@ -71,21 +69,20 @@ function DrumMachine() {
                     <input type="range"  orient="vertical" id="line" min="0" max="100" onInput={changeVolume}/>
                 </div>
                 <div id="pad-grid">
-                    <div name="Kick" padkey="Q" className="drum-pad" sound={kick} onClick={changeClip}><h3>Q</h3></div>
-                    <div name="Snare" padkey="W" className="drum-pad" sound={snare} onClick={changeClip}><h3>W</h3></div>
-                    <div name="Hand Clap" padkey="E" className="drum-pad" sound={handclap} onClick={changeClip}><h3>E</h3></div>
-                    <div name="Closed Hi-Hat" padkey="A" className="drum-pad" sound={closedhihat} onClick={changeClip}><h3>A</h3></div>
-                    <div name="Open Hi-Hat" padkey="S" className="drum-pad" sound={openhihat} onClick={changeClip}><h3>S</h3></div>
-                    <div name="Cymbal" padkey="D" className="drum-pad" sound={cymbal} onClick={changeClip}><h3>D</h3></div>
-                    <div name="Low Tom" padkey="Z" className="drum-pad" sound={lowtom} onClick={changeClip}><h3>Z</h3></div>
-                    <div name="High Tom" padkey="X" className="drum-pad" sound={hightom} onClick={changeClip}><h3>X</h3></div>
-                    <div name="Clave" padkey="C" className="drum-pad" sound={clave} onClick={changeClip}><h3>C</h3></div>
+                    <div name="Kick" className="drum-pad" onClick={(e) => {playClip(e.target, kick)}}><h3>Q</h3></div>
+                    <div name="Snare" className="drum-pad" onClick={(e) => {playClip(e.target, snare)}}><h3>W</h3></div>
+                    <div name="Hand Clap" className="drum-pad" onClick={(e) => {playClip(e.target, handclap)}}><h3>E</h3></div>
+                    <div name="Closed Hi-Hat" className="drum-pad" onClick={(e) => {playClip(e.target, closedhihat)}}><h3>A</h3></div>
+                    <div name="Open Hi-Hat" className="drum-pad" onClick={(e) => {playClip(e.target, openhihat)}}><h3>S</h3></div>
+                    <div name="Cymbal" className="drum-pad" onClick={(e) => {playClip(e.target, cymbal)}}><h3>D</h3></div>
+                    <div name="Low Tom" className="drum-pad" onClick={(e) => {playClip(e.target, lowtom)}}><h3>Z</h3></div>
+                    <div name="High Tom" className="drum-pad" onClick={(e) => {playClip(e.target, hightom)}}><h3>X</h3></div>
+                    <div name="Clave" className="drum-pad" onClick={(e) => {playClip(e.target, clave)}}><h3>C</h3></div>
                 </div>
                 </div>
             </div>
         </div>
-    );
-      
+    );  
 }
 
 export default DrumMachine
